2a9b8b8897d2be5b097684e11e9f614edab0029d
Fluxiitis <36551429+Fluxiitis@users.noreply.github.com>
2018-06-16 23:58:44 +0300

    Update Katarina.lua

--- Katarina.lua
+++ Katarina.lua
@@ -6,8 +6,8 @@ function PrintMsg(msg)
 	PrintChat("<font color=\"#ff0000\"><b>[Katarina]</b></font> <font color=\"#ffffff\">"..msg.."</font>")
 end
 
-if FileExist(LIB_PATH..'TRPrediction.lua') then
-  	require('TRPrediction')
+if FileExist(LIB_PATH..'TPred.lua') then
+  	require('TPred')
 else
   	print("Downloading TRPrediction, please don't press F9")
     DelayAction(function() DownloadFile("https://raw.githubusercontent.com/Project4706/BoL/master/TRPrediction.lua".."?rand="..math.random(1,10000), LIB_PATH.."TRPrediction.lua", function () print("Successfully downloaded TRPrediction. Press F9 twice.") end) end, 3) 
